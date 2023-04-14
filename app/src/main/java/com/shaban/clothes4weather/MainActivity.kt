6bf285837d9ce37db992809c042e127323e0cf69
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-14 13:26:54 +0200

    add statusBarTheme()

--- app/src/main/java/com/shaban/clothes4weather/MainActivity.kt
+++ app/src/main/java/com/shaban/clothes4weather/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.shaban.clothes4weather
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        statusBarTheme()
+    }
+
+    @Suppress("DEPRECATION")
+    private fun statusBarTheme() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        window.statusBarColor = Color.TRANSPARENT
+
+        if (isDarkTheme()) {
+            window.decorView.systemUiVisibility =
+                window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+        } else {
+            window.decorView.systemUiVisibility =
+                window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+        val decorView = window.decorView
+        decorView.systemUiVisibility =
+            decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+    }
+
+    private fun isDarkTheme(): Boolean {
+        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        return currentNightMode == Configuration.UI_MODE_NIGHT_YES
     }
 }
