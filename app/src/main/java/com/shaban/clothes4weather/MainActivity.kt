ce7e049492bef62b8f3f3c6f3425d717ec082e79
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-14 16:36:39 +0200

    add splash screen for light and dark theme

--- app/src/main/java/com/shaban/clothes4weather/MainActivity.kt
+++ app/src/main/java/com/shaban/clothes4weather/MainActivity.kt
@@ -5,10 +5,12 @@ import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         statusBarTheme()
