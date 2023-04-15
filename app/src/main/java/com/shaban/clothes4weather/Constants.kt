e2916580a4911107fd09e6f096dbc9afe7ee0749
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-15 02:02:14 +0200

    implement all permissions functions

---
+++ app/src/main/java/com/shaban/clothes4weather/Constants.kt
@@ -0,0 +1,5 @@
+package com.shaban.clothes4weather
+
+object Constants {
+    const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+}
--- app/src/main/java/com/shaban/clothes4weather/OnboardingFragment.kt
+++ app/src/main/java/com/shaban/clothes4weather/OnboardingFragment.kt
@@ -1,8 +1,23 @@
 package com.shaban.clothes4weather
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.shaban.clothes4weather.databinding.FragmentOnboardingBinding
 
 class OnboardingFragment : BaseFragment<FragmentOnboardingBinding>() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     override val TAG: String
         get() = this::class.simpleName.toString()
 
@@ -10,6 +25,93 @@ class OnboardingFragment : BaseFragment<FragmentOnboardingBinding>() {
         FragmentOnboardingBinding.inflate(layoutInflater)
 
     override fun setup() {
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireContext())
+        addCallBacks()
+    }
 
+    private fun addCallBacks() {
+        binding.getLocationButton.setOnClickListener {
+            getCurrentLocation()
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            requireContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+            requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun getCurrentLocation() {
+
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+
+                if (ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermission()
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener {
+                    val location: Location? = it.result
+                    if (location == null)
+                        Toast.makeText(context, "Null Recieved!!", Toast.LENGTH_SHORT).show()
+                    else {
+                        Toast.makeText(context, "Get Success!!", Toast.LENGTH_SHORT).show()
+                        // send lat and long to the home fragment
+                    }
+                }
+            } else {
+                Toast.makeText(context, "Granted!!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            Constants.PERMISSION_REQUEST_ACCESS_LOCATION
+        )
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == Constants.PERMISSION_REQUEST_ACCESS_LOCATION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(context, "Granted!!", Toast.LENGTH_SHORT).show()
+                getCurrentLocation()
+            } else {
+                Toast.makeText(context, "Denied!!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
