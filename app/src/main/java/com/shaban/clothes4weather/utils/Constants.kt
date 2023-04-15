9d3dfd71026eca13058d9fb4009ef93b79ecb019
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-15 22:28:04 +0200

    add URL segments

--- app/src/main/java/com/shaban/clothes4weather/utils/Constants.kt
+++ app/src/main/java/com/shaban/clothes4weather/utils/Constants.kt
@@ -2,5 +2,13 @@ package com.shaban.clothes4weather.utils
 
 object Constants {
     const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
-    const val BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+    const val SCHEME = "https"
+    const val HOST = "api.openweathermap.org"
+    const val PATH_SEGMENT_ONE = "data"
+    const val PATH_SEGMENT_TWO = "2.5"
+    const val PATH_SEGMENT_THREE = "weather"
+    const val LONGITUDE_QUERY_PARAM = "lon"
+    const val LATITUDE_QUERY_PARAM = "lat"
+    const val API_KEY_QUERY_PARAM = "appid"
+    const val API_KEY_VALUE = "3981ad69dd8900d87fc6a663bfa37581"
 }
