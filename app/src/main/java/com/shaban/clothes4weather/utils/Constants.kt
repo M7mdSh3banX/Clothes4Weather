73ce63a14866d08956ae6cdfc19678b82c6ea262
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-15 21:57:25 +0200

    add base url of weather API

--- app/src/main/java/com/shaban/clothes4weather/utils/Constants.kt
+++ app/src/main/java/com/shaban/clothes4weather/utils/Constants.kt
@@ -2,4 +2,5 @@ package com.shaban.clothes4weather.utils
 
 object Constants {
     const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 }
