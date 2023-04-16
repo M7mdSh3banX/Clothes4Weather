89e8870d360a20f7145334c6f8a9362ca9595f1c
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-16 20:23:13 +0200

    apply implementation of theme switch button

--- app/src/main/java/com/shaban/clothes4weather/ui/HomeFragment.kt
+++ app/src/main/java/com/shaban/clothes4weather/ui/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.shaban.clothes4weather.ui
 
+import androidx.appcompat.app.AppCompatDelegate
 import com.shaban.clothes4weather.data.source.RemoteDataSource
 import com.shaban.clothes4weather.databinding.FragmentHomeBinding
 import com.shaban.clothes4weather.ui.base.BaseFragment
@@ -15,7 +16,24 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
 
     override fun setup() {
+        SharedPreferencesUtil.initPreferencesUtil(requireContext())
         RemoteDataSource.makeRequestUsingOKHTTP()
+        isNightMode()
     }
 
+    private fun isNightMode() {
+        if (SharedPreferencesUtil.isNightMode!!) {
+            binding.switchMode.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        binding.switchMode.setOnClickListener {
+            if (SharedPreferencesUtil.isNightMode!!) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                SharedPreferencesUtil.isNightMode = false
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                SharedPreferencesUtil.isNightMode = true
+            }
+        }
+    }
 }
