dd37c1418f7e1c9b567fba95a7bd6f7ce0b9064c
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-17 16:09:02 +0200

    update the quality of weather status icon

--- app/src/main/java/com/shaban/clothes4weather/data/domain/IconWeatherStatus.kt
+++ app/src/main/java/com/shaban/clothes4weather/data/domain/IconWeatherStatus.kt
@@ -5,6 +5,6 @@ import com.shaban.clothes4weather.utils.Constants.ICON_BASE_URL
 object IconWeatherStatus {
 
     fun iconCode(code: String): String {
-        return "${ICON_BASE_URL}$code@2x.png"
+        return "${ICON_BASE_URL}$code@4x.png"
     }
 }
