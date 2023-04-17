c33f1c20af689c37e13df9e77e0152a83ebb9560
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-17 03:10:58 +0200

    add SummerClothes data class

---
+++ app/src/main/java/com/shaban/clothes4weather/data/domain/SummerClothes.kt
@@ -0,0 +1,18 @@
+package com.shaban.clothes4weather.data.domain
+
+import com.shaban.clothes4weather.R
+
+data class SummerClothes(val image: Int)
+
+val summerClothes = listOf(
+    R.drawable.summer_outfit1,
+    R.drawable.summer_outfit2,
+    R.drawable.summer_outfit3,
+    R.drawable.summer_outfit4,
+    R.drawable.summer_outfit5,
+    R.drawable.summer_outfit6,
+    R.drawable.summer_outfit7,
+    R.drawable.summer_outfit8,
+    R.drawable.summer_outfit9,
+    R.drawable.summer_outfit10
+)
