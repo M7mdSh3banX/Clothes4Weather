c6894c39b7ebe47ebe26d4abdc8f7a1c0e1f0e0b
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-17 16:42:21 +0200

    enhance api url

--- app/src/main/java/com/shaban/clothes4weather/data/source/RemoteDataSource.kt
+++ app/src/main/java/com/shaban/clothes4weather/data/source/RemoteDataSource.kt
@@ -3,16 +3,9 @@ package com.shaban.clothes4weather.data.source
 import android.util.Log
 import com.google.gson.Gson
 import com.shaban.clothes4weather.data.models.WeatherResponse
-import com.shaban.clothes4weather.utils.Constants.API_KEY_QUERY_PARAM
-import com.shaban.clothes4weather.utils.Constants.API_KEY_VALUE
-import com.shaban.clothes4weather.utils.Constants.HOST
-import com.shaban.clothes4weather.utils.Constants.LATITUDE_QUERY_PARAM
-import com.shaban.clothes4weather.utils.Constants.LONGITUDE_QUERY_PARAM
-import com.shaban.clothes4weather.utils.Constants.PATH_SEGMENT_ONE
-import com.shaban.clothes4weather.utils.Constants.PATH_SEGMENT_THREE
-import com.shaban.clothes4weather.utils.Constants.PATH_SEGMENT_TWO
-import com.shaban.clothes4weather.utils.Constants.SCHEME
-import com.shaban.clothes4weather.utils.SharedPreferencesUtil
+import com.shaban.clothes4weather.utils.Constants
+import com.shaban.clothes4weather.utils.SharedPreferencesUtil.latitude
+import com.shaban.clothes4weather.utils.SharedPreferencesUtil.longitude
 import okhttp3.*
 import java.io.IOException
 
@@ -21,20 +14,10 @@ object RemoteDataSource {
     private val client = OkHttpClient()
     fun makeRequestUsingOKHTTP(callBack: RemoteDataSourceInterface) {
 
-
-        val url = HttpUrl
-            .Builder()
-            .scheme(SCHEME)
-            .host(HOST)
-            .addPathSegment(PATH_SEGMENT_ONE)
-            .addPathSegment(PATH_SEGMENT_TWO)
-            .addPathSegment(PATH_SEGMENT_THREE)
-            .addQueryParameter(LATITUDE_QUERY_PARAM, SharedPreferencesUtil.latitude.toString())
-            .addQueryParameter(LONGITUDE_QUERY_PARAM, SharedPreferencesUtil.longitude.toString())
-            .addQueryParameter(API_KEY_QUERY_PARAM, API_KEY_VALUE)
+        val request = Request.Builder()
+            .url("${Constants.BASE_URL}/weather?lat=$latitude&lon=$longitude&appid=${Constants.API_KEY_VALUE}")
             .build()
 
-        val request = Request.Builder().url(url).build()
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 callBack.onError(e.message!!)
--- app/src/main/java/com/shaban/clothes4weather/utils/Constants.kt
+++ app/src/main/java/com/shaban/clothes4weather/utils/Constants.kt
@@ -2,14 +2,7 @@ package com.shaban.clothes4weather.utils
 
 object Constants {
     const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
-    const val SCHEME = "https"
-    const val HOST = "api.openweathermap.org"
-    const val PATH_SEGMENT_ONE = "data"
-    const val PATH_SEGMENT_TWO = "2.5"
-    const val PATH_SEGMENT_THREE = "weather"
-    const val LONGITUDE_QUERY_PARAM = "lon"
-    const val LATITUDE_QUERY_PARAM = "lat"
-    const val API_KEY_QUERY_PARAM = "appid"
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5"
     const val API_KEY_VALUE = "3981ad69dd8900d87fc6a663bfa37581"
     const val ICON_BASE_URL = "https://openweathermap.org/img/wn/"
 }
