63c72ee9b23f7e25a75ff376123abec41df4c2bf
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-17 03:14:37 +0200

    add WinterClothes data class

---
+++ app/src/main/java/com/shaban/clothes4weather/data/domain/WinterClothes.kt
@@ -0,0 +1,18 @@
+package com.shaban.clothes4weather.data.domain
+
+import com.shaban.clothes4weather.R
+
+data class WinterClothes(val image: Int)
+
+val winterClothes = listOf(
+    R.drawable.winter_outfit1,
+    R.drawable.winter_outfit2,
+    R.drawable.winter_outfit3,
+    R.drawable.winter_outfit4,
+    R.drawable.winter_outfit5,
+    R.drawable.winter_outfit6,
+    R.drawable.winter_outfit7,
+    R.drawable.winter_outfit8,
+    R.drawable.winter_outfit9,
+    R.drawable.winter_outfit10
+)
