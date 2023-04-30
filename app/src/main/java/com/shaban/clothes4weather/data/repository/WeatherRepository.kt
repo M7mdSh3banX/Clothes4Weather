94a93ae3667e27d215e543a3ee4d9ee053907ae2
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-30 07:56:02 +0300

    update getWeather()

--- app/src/main/java/com/shaban/clothes4weather/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/shaban/clothes4weather/data/repository/WeatherRepository.kt
@@ -1,8 +1,15 @@
 package com.shaban.clothes4weather.data.repository
 
+import com.shaban.clothes4weather.BuildConfig
 import com.shaban.clothes4weather.data.source.RemoteDataSource
+import com.shaban.clothes4weather.utils.SharedPreferencesUtil.latitude
+import com.shaban.clothes4weather.utils.SharedPreferencesUtil.longitude
 
 class WeatherRepository {
 
-    fun getWeather() = RemoteDataSource.weatherService.getWeather()
+    fun getWeather() = RemoteDataSource.weatherService.getWeather(
+        latitude,
+        longitude,
+        BuildConfig.API_KEY
+    )
 }
--- app/src/main/java/com/shaban/clothes4weather/data/source/RemoteDataSourceInterface.kt
+++ app/src/main/java/com/shaban/clothes4weather/data/source/RemoteDataSourceInterface.kt
@@ -1,12 +1,17 @@
 package com.shaban.clothes4weather.data.source
 
-import com.shaban.clothes4weather.BuildConfig
 import com.shaban.clothes4weather.data.models.WeatherResponse
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RemoteDataSourceInterface {
 
-    @GET("weather?lat=${29.3088779}&lon=${30.8259737}&appid=${BuildConfig.API_KEY}")
-    fun getWeather(): Single<WeatherResponse>
+    @GET("weather")
+    fun getWeather(
+        @Query("lat") latitude: Float?,
+        @Query("lon") longitude: Float?,
+        @Query("appid") apiKey: String
+    ): Single<WeatherResponse>
+
 }
