e5e2a46cf30abec1ed6632d6a85e9253b1a3d9b6
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-30 03:10:54 +0300

    convert api service from okhttp to retrofit

--- app/src/main/java/com/shaban/clothes4weather/data/source/RemoteDataSource.kt
+++ app/src/main/java/com/shaban/clothes4weather/data/source/RemoteDataSource.kt
@@ -1,39 +1,17 @@
 package com.shaban.clothes4weather.data.source
 
-import android.util.Log
-import com.google.gson.Gson
-import com.shaban.clothes4weather.BuildConfig
-import com.shaban.clothes4weather.data.models.WeatherResponse
-import com.shaban.clothes4weather.utils.Constants
-import com.shaban.clothes4weather.utils.SharedPreferencesUtil.latitude
-import com.shaban.clothes4weather.utils.SharedPreferencesUtil.longitude
-import okhttp3.*
-import java.io.IOException
+import com.shaban.clothes4weather.utils.Constants.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 object RemoteDataSource {
 
-    private val client = OkHttpClient()
-    fun makeRequestUsingOKHTTP(callBack: RemoteDataSourceInterface) {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .build()
 
-        val request = Request.Builder()
-            .url("${Constants.BASE_URL}/weather?lat=$latitude&lon=$longitude&appid=${BuildConfig.API_KEY}")
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                callBack.onError(e.message!!)
-                Log.i("TAG_TEST", "Fail Response: ${e.message}")
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    response.body?.string()?.let {
-                        val responseResult = Gson().fromJson(it, WeatherResponse::class.java)
-                        callBack.onSuccess(responseResult)
-                        Log.i("TAG_TEST", responseResult.toString())
-                    }
-                }
-            }
-        })
-    }
+    val weatherService = retrofit.create(RemoteDataSourceInterface::class.java)
 }
--- app/src/main/java/com/shaban/clothes4weather/data/source/RemoteDataSourceInterface.kt
+++ app/src/main/java/com/shaban/clothes4weather/data/source/RemoteDataSourceInterface.kt
@@ -1,8 +1,12 @@
 package com.shaban.clothes4weather.data.source
 
+import com.shaban.clothes4weather.BuildConfig
 import com.shaban.clothes4weather.data.models.WeatherResponse
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
 
 interface RemoteDataSourceInterface {
-    fun onSuccess(weatherResponse: WeatherResponse)
-    fun onError(messageError: String)
+
+    @GET("weather?lat=${29.3088779}&lon=${30.8259737}&appid=${BuildConfig.API_KEY}")
+    fun getWeather(): Single<WeatherResponse>
 }
