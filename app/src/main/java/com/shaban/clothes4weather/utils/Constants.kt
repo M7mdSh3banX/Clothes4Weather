f58ed84764d992be535800d08167b136c98c58de
Mohamed-Shaban <ms2951@fayoum.edu.eg>
2023-04-30 06:55:07 +0300

    add '/' to BASE_URL

--- app/src/main/java/com/shaban/clothes4weather/utils/Constants.kt
+++ app/src/main/java/com/shaban/clothes4weather/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.shaban.clothes4weather.utils
 
 object Constants {
     const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
-    const val BASE_URL = "https://api.openweathermap.org/data/2.5"
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     const val ICON_BASE_URL = "https://openweathermap.org/img/wn/"
 }
